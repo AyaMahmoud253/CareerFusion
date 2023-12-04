10c99bbe7367d022d2709ee892d9c982c39c46c4
AyaMahmoud253 <119099432+AyaMahmoud253@users.noreply.github.com>
2023-12-04 11:39:20 +0200

    map

--- Web_API/Web_API/Program.cs
+++ Web_API/Web_API/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Web_API.helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Accessing configuration
+var configuration = builder.Configuration;
+var jwtSettings = new JWT();
+configuration.GetSection("JWT").Bind(jwtSettings);
+
+// Register the JWT settings in the DI container
+builder.Services.AddSingleton(jwtSettings);
 
 var app = builder.Build();
 
@@ -17,9 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
