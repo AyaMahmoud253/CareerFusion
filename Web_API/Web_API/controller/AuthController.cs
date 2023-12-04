a468faa78707283b2a23a002d23e4f1343ba0ad2
AyaMahmoud253 <119099432+AyaMahmoud253@users.noreply.github.com>
2023-12-04 18:54:03 +0200

    Controller1

---
+++ Web_API/Web_API/controller/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_API.Models;
+using Web_API.services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+namespace Web_API.controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync([FromBody] RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _authService.RegisterAsync(model);
+
+            if (!result.IsAuthenticated)
+                return BadRequest(result.Message);
+
+            return Ok(result);
+        }
+           
+    }
+}
